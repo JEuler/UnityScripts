fa53e7ea849e1614fbe6cded27ad1dd6fbfe4b2a
Ivan Terekhin <i.terhin@gmail.com>
2016-04-17 22:37:26 +0800

    Exception logger script added

---
+++ Assets/Scripts/ExceptionLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class ExceptionLogger : MonoBehaviour {
+
+	private StreamWriter SW;
+	public string LogFileName = "log.txt";
+
+	// Use this for initialization
+	void Start() {
+		DontDestroyOnLoad( gameObject );
+		SW = new StreamWriter( Application.persistentDataPath + "/" + LogFileName );
+	}
+
+	// Unity 5 api
+	void OnEnable() {
+		Application.logMessageReceived += HandleLog;
+	}
+
+	void OnDisable() {
+		Application.logMessageReceived -= HandleLog;
+	}
+
+	void HandleLog(string logString, string stackTrace, LogType type) {
+		if (type == LogType.Exception || type == LogType.Error) {
+			SW.WriteLine("Logged at: " + DateTime.Now + " - Log Desc: " + logString + " - Trace: " + stackTrace + "- Type: " + type);
+		}
+	}
+
+	void OnDestroy() {
+		SW.Close();
+	}
+}
