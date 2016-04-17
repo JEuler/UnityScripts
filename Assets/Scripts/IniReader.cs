4bee1172e25a5e423d57c6c590ad8a7677cb7d6c
Ivan Terekhin <i.terhin@gmail.com>
2016-04-17 22:37:36 +0800

    Ini Reader added

---
+++ Assets/Scripts/IniReader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class IniReader : MonoBehaviour {
+	public static Dictionary<string, string> ReadINIFile( string Filename ) {
+		if ( !File.Exists( Filename ) )
+			return null;
+		Dictionary<string, string> IniFile = new Dictionary<string, string>();
+		using ( StreamReader SR = new StreamReader( Filename ) ) {
+			string Line;
+			while ( !string.IsNullOrEmpty( Line = SR.ReadLine() ) ) {
+				Line.Trim();
+				string[] Parts = Line.Split( new char[] { '=' } );
+				IniFile.Add( Parts[ 0 ].Trim(), Parts[ 1 ].Trim() );
+			}
+		}
+		return IniFile;
+	}
+}
