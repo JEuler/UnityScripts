ffa0271565a0452348fd4be320259ae3619778e7
Ivan Terekhin <i.terhin@gmail.com>
2016-04-18 20:41:40 +0800

    Gizmo shower (book) added

---
+++ Assets/Scripts/GizmoShower.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GizmoShower : MonoBehaviour {
+
+	public bool Show = true;
+	public string Icon = string.Empty;
+
+	// Range of "sight"
+	[Range( 0f, 100f )]
+	public float Range = 10f;
+
+	void OnDrawGizmos() {
+		if ( !Show )
+			return;
+		Gizmos.DrawIcon( transform.position, Icon, true );
+
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere( transform.position, Range );
+
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine( transform.position, transform.position + transform.forward * Range );
+	}
+}
