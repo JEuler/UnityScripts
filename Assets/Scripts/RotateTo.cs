06ec482b5436b7ef7faca399e5a317c2b2385747
Ivan Terekhin <i.terhin@gmail.com>
2016-04-20 02:42:21 +0800

    Rotate To Script

---
+++ Assets/Scripts/RotateTo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTo : MonoBehaviour {
+
+	private Transform CurrentTransform = null;
+	public float RotationSpeed = 60f;
+
+	public Transform Target;
+	public float Damping = 55f;
+
+	void Awake() {
+		CurrentTransform = GetComponent<Transform>();
+	}
+
+	void Update() {
+		RotateSlerp();
+	}
+
+	private void RotateBasic() {
+		Quaternion DestRotation = Quaternion.LookRotation( Target.position - CurrentTransform.position, Vector3.up );
+		CurrentTransform.rotation = Quaternion.RotateTowards( CurrentTransform.rotation, DestRotation, RotationSpeed * Time.deltaTime );
+	}
+
+	private void RotateSlerp() {
+		Quaternion DestRotation = Quaternion.LookRotation( Target.position - CurrentTransform.position, Vector3.up );
+		Quaternion SmoothRotation = Quaternion.Slerp( transform.rotation, DestRotation, 1f - Time.deltaTime * Damping );
+	}
+}
