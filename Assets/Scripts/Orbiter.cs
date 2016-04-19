41a03db7e3c6cfc596b2418639c643be2185998c
Ivan Terekhin <i.terhin@gmail.com>
2016-04-19 19:33:11 +0800

    Basic orbiter script

---
+++ Assets/Scripts/Orbiter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Orbiter : MonoBehaviour {
+	public Transform Pivot = null;
+	private Transform CurrentTransform = null;
+	private Quaternion DestinationRotation = Quaternion.identity;
+	// Distance from pivot (constraints)
+	public float PivotDistance = 5f;
+	public float RotationSpeed = 10f;
+	private float RotX = 0f;
+	private float RotY = 0f;
+
+	void Awake() {
+		CurrentTransform = GetComponent<Transform>();
+	}
+
+	void Update() {
+		float Horizontal = CrossPlatformInputManager.GetAxis( "Horizontal" );
+		float Vertical = CrossPlatformInputManager.GetAxis( "Vertical" );
+
+		RotX += Vertical * Time.deltaTime * RotationSpeed;
+		RotY += Horizontal * Time.deltaTime * RotationSpeed;
+
+		Quaternion YRot = Quaternion.Euler( 0f, RotY, 0f );
+		DestinationRotation = YRot * Quaternion.Euler( RotX, 0f, 0f );
+
+		CurrentTransform.rotation = DestinationRotation;
+		CurrentTransform.position = Pivot.position + CurrentTransform.rotation * Vector3.forward * -PivotDistance;
+	}
+}
