475b034651e7d97686b4d7a9c26278aeb50e0773
Ivan Terekhin <i.terhin@gmail.com>
2016-04-23 00:34:46 +0800

    Billboard

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour {
+	private Transform CurrentTransform = null;
+
+	// Use this for initialization
+	void Start() {
+		CurrentTransform = transform;
+	}
+
+	// Update is called once per frame
+	void LateUpdate() {
+		Vector3 LookAtDir = new Vector3( Camera.main.transform.position.x - CurrentTransform.position.x, 0,
+			Camera.main.transform.position.z - CurrentTransform.position.z );
+		CurrentTransform.rotation = Quaternion.LookRotation( LookAtDir.normalized, Vector3.up );
+	}
+}
